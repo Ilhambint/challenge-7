85bf5a34e5c3bceef058123a9c0850e147b5bcfc
Ilhambint <ilhambintangp7@gmail.com>
2022-05-27 03:21:36 +0700

    first

--- pages/chart.js
+++ pages/chart.js
@@ -1,4 +1,27 @@
 import React from 'react'
+import Head from 'next/dist/shared/lib/head'
+
+import { 
+    chart
+}
+
+const data = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
 
 export default function chart() {
   return (
