38e46e1c66548e6cbc42cf80c721d61c451aa412
Ilhambint <ilhambintangp7@gmail.com>
2022-05-27 22:06:55 +0700

    second

--- pages/chart.js
+++ pages/chart.js
@@ -1,30 +1,77 @@
 import React from 'react'
-import Head from 'next/dist/shared/lib/head'
+import Head from 'next/head'
+import styles from '../styles/chart.module.css'
+import {Chart as ChartJS, RadialLinearScale, ArcElement, Tooltip, Legend} from 'chart.js';
+import {PolarArea} from 'react-chartjs-2';
 
-import { 
-    chart
-}
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 
 const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
-  };
+  labels: [
+    'Eating',
+    'Drinking',
+    'Sleeping',
+    'Designing',
+    'Coding',
+    'Cycling',
+    'Running'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 90, 81, 56, 55, 40],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }, {
+    label: 'My Second Dataset',
+    data: [28, 48, 40, 19, 96, 27, 100],
+    fill: true,
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    pointBackgroundColor: 'rgb(54, 162, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
+  }]
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {
+    animations: {
+      tension: {
+        duration: 1000,
+        easing: 'linear',
+        from: 1,
+        to: 0,
+        loop: true
+      }
+    },
+    scales: {
+      y: { // defining min and max so hiding the dataset does not change scale range
+        min: 0,
+        max: 100
+      }
+    }
+  }
+};
 
 export default function chart() {
-  return (
-    <div>chart</div>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Chart</title>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            </Head>
+            <div className={styles.chart}>
+              <h1 className={styles.heading}>Radar Chart</h1>
+                <PolarArea data={data}/>
+            </div>
+        </div>
+    )
 }
--- pages/input.js
+++ pages/input.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Head from 'next/head'
+import axios from 'axios'
 
 export default function input() {
-  return (
-    <div>input</div>
-  )
+    const url = ""
+    const [data, setData] = useState({name: "", age: "", email: ""});
+
+    function handle(e) {
+        const newdata = {
+            ...data
+        }
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        console.log(newdata)
+    }
+    return (
+        <div>
+            <div>
+                <form>
+                    <input
+                        onChange={(e) => handle(e)}
+                        id="name"
+                        value={data.name}
+                        placeholder="name"
+                        type="text"></input> <br/>
+
+                    <input
+                        onChange={(e) => handle(e)}
+                        id="age"
+                        value={data.afe}
+                        placeholder="age"
+                        type="text"></input><br/>
+
+                    <input
+                        onChange={(e) => handle(e)}
+                        id="email"
+                        value={data.email}
+                        placeholder="email"
+                        type="email"></input>
+                </form>
+            </div>
+
+            
+
+        </div>
+
+    )
 }
